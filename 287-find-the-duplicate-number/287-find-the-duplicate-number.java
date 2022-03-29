57264105e61004661092d8d95f59f2dc1f5ccaa1
PoorviGpt <83346786+PoorviGpt@users.noreply.github.com>
2022-03-29 23:38:23 +0530

    Time: 47 ms (13.60%), Space: 75.2 MB (72.39%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> numsSet=new HashSet<>();
+        /*Set<Integer> numsSet=new HashSet<>();
         for(Integer num :nums)
         {
             if(numsSet.contains(num))
@@ -11,6 +11,20 @@ class Solution {
             {
                 numsSet.add(num);
             }
+        }*/
+        
+        Arrays.sort(nums);
+        int i=0;
+        int j=i+1;
+        while(j < nums.length)
+        {
+            //System.out.println("i ^ j = " + (nums[i] ^ nums[j]));
+            if((nums[i] ^ nums[j])==0)
+            {
+                return nums[i];
+            }
+            i++;
+            j++;
         }
         return 0;
     }
