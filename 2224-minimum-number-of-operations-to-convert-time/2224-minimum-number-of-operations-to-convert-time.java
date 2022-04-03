87a88facdc6f661c716d36c039c9de255674ac36
PoorviGpt <83346786+PoorviGpt@users.noreply.github.com>
2022-04-03 10:05:05 +0530

    Time: 3 ms (25.00%), Space: 42.4 MB (25.00%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -10,25 +10,6 @@ class Solution {
         int corrmin=h*60+Integer.parseInt(correctTimeStr[1]);
         int minDifference=Math.abs(corrmin-currmin);
          minOpr=minDifference/60+((minDifference%60)/15)+((minDifference%15)/5)+(minDifference%5);
-        /*int hrsDifference=Integer.parseInt(correctTimeStr[0])-Integer.parseInt(currentTimeStr[0]);
-        int minDifference=Integer.parseInt(correctTimeStr[1])-Integer.parseInt(currentTimeStr[1]);
-        
-        if(minDifference >= 0 && hrsDifference>=0)
-        {
-            minOpr=minOpr+hrsDifference;
-            minOpr=minOpr+(minDifference/15)+((minDifference%15)/5)+(minDifference%5);
-        }
-        else
-        {
-            
-            minOpr=minOpr+hrsDifference;
-            
-            minDifference=59-Math.abs(minDifference);
-            minOpr=minOpr+(minDifference/15)+((minDifference%15)/5)+(minDifference%5);
-        }
-         
-            
-        */
         
         
         return minOpr;
