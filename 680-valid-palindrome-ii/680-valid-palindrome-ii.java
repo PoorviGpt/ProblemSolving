e835b5e9dd32024d011ba44b74140a59eae9be56
PoorviGpt <83346786+PoorviGpt@users.noreply.github.com>
2022-04-03 22:00:01 +0530

    Time: 11 ms (45.76%), Space: 54.5 MB (48.80%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int mismatchCount=0;
+        return checkPalindrome(s,0);
+    }
+    public boolean checkPalindrome(String s,int mismatchCount) {
+        int i=0;int j=s.length()-1;
+        while(i < j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+               mismatchCount++;
+               if((i+1 < j && s.charAt(i+1) != s.charAt(j)) &&
+               (j-1 > i && s.charAt(i) != s.charAt(j-1)))
+               {
+                    return false;
+               }
+               else if((i+1 < j && s.charAt(i+1) == s.charAt(j)) &&
+               (j-1 > i && s.charAt(i) == s.charAt(j-1)))
+               {
+                   return checkPalindrome(s.substring(i+1,j+1),mismatchCount) || checkPalindrome(s.substring(i,j),mismatchCount);
+               }
+               else if(j-1 > i && s.charAt(i) == s.charAt(j-1))
+               {
+                   return checkPalindrome(s.substring(i,j),mismatchCount);
+               }
+               else if(i+1 < j && s.charAt(i+1) == s.charAt(j))
+               {
+                   return checkPalindrome(s.substring(i+1,j+1),mismatchCount);
+               }
+            }
+            i++;
+            j--;
+        }
+        
+        //System.out.println("mismatchCount "+mismatchCount);
+        if(mismatchCount > 1)
+            return false;
+        return true;
+    }
+}
