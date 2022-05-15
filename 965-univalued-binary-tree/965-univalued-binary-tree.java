f314176467beb8d859c011ca211bd2f585bb5b92
PoorviGpt <83346786+PoorviGpt@users.noreply.github.com>
2022-05-15 12:10:28 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (95.98%) - LeetHub

---
+++ 965-univalued-binary-tree/965-univalued-binary-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root==null)
+            return false;
+    
+       
+        return univalue(root,root.val);
+    }
+    private boolean univalue(TreeNode root, int data)
+    {
+        if(root == null)
+            return true;
+
+        if(root.val!=data)
+            return false;
+
+        boolean left=  univalue(root.left,data);
+        boolean right = univalue(root.right,data);
+
+        return left && right;
+    }
+}
